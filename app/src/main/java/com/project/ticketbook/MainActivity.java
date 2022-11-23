167cb7eabdf950b6884aa11cae6b46c82c3246fe
Raag <raag.gautam@gmail.com>
2022-11-23 18:16:23 +0530

    Changed class Hash to LoginUtils. Added LoginUtils.salter(String,string) Added LoginUtils.User class

--- app/src/main/java/com/project/ticketbook/MainActivity.java
+++ app/src/main/java/com/project/ticketbook/MainActivity.java
@@ -1,7 +1,8 @@
 package com.project.ticketbook;
 
-import androidx.appcompat.app.AppCompatActivity;
+import com.project.ticketbook.LoginUtils.*;
 
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
@@ -10,5 +11,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initData_(LoginUtils.users);
+    }
+
+    void initData_(LoginUtils.User users[]){
+        users[0] = new User("abc.def@xyz.com","abcdefgh","12-23-12");
+        users[1] = new User("ghii@xyz.com","abcdefgg","12-23-12");
+        users[2] = new User("sc@xyz.com","12345678","12-23-12");
+        users[3] = new User("mouse@xyz.com","12345677","12-23-12");
+        users[4] = new User("music@xyz.com","GuitarsAreGuitars","12-23-12");
     }
 }
